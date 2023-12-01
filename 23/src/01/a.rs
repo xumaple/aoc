16e79e685f9025973379931b1a56311fd92df27e
Maple Xu <mapxu@microsoft.com>
2023-12-01 19:25:51 +0000

    01a

--- 23/src/01/a.rs
+++ 23/src/01/a.rs
@@ -1,17 +1,24 @@
 use std::fs::File;
-use std::io::{self, BufReader, Lines, BufRead};
+use std::io::{self, BufRead, BufReader, Lines};
 use std::path::Path;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut sum = 0;
     for l in read_lines("src/01/input.txt")? {
-
+        let l = l?;
+        let c1 = l.chars().find(|c| c.is_digit(10));
+        let c2 = l.chars().rfind(|c| c.is_digit(10));
+        sum += 10 * c1.unwrap().to_digit(10).unwrap() + c2.unwrap().to_digit(10).unwrap();
     }
 
+    println!("{sum}");
     Ok(())
 }
 
 fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(BufReader::new(file).lines())
 }
--- 23/src/template.rs
+++ 23/src/template.rs
@@ -1,17 +1,17 @@
 use std::fs::File;
-use std::io::{self, BufReader, Lines, BufRead};
+use std::io::{self, BufRead, BufReader, Lines};
 use std::path::Path;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    for l in read_lines("src/01/input.txt")? {
-
-    }
+    for l in read_lines("src/<>/input.txt")? {}
 
     Ok(())
 }
 
 fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(BufReader::new(file).lines())
 }
