7fb66d1a6a1a8a5dbe510d0a03647a763ffc97ff
Maple Xu <mapxu@microsoft.com>
2023-12-06 18:57:33 +0000

    03

---
+++ 23/src/03/a.rs
@@ -0,0 +1,78 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Lines};
+use std::path::Path;
+
+fn exists(sorted_list: &Vec<usize>, min_val: usize, max_val: usize) -> bool {
+    for &x in sorted_list {
+        if x >= min_val {
+            if x <= max_val {
+                return true;
+            }
+            if x > max_val {
+                break;
+            }
+        }
+    }
+    return false;
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = "src/03/input.txt";
+    let symbol_locs: Vec<_> = read_lines(filename)?
+        .into_iter()
+        .map(|line| {
+            Ok(line?
+                .char_indices()
+                .filter_map(|(i, c)| {
+                    if c.is_digit(10) || c == '.' {
+                        None
+                    } else {
+                        Some(i)
+                    }
+                })
+                .collect::<Vec<usize>>())
+        })
+        .collect::<Result<Vec<_>, Box<dyn Error>>>()?;
+    let sum: i32 = read_lines(filename)?
+        .into_iter()
+        .enumerate()
+        .map(|(curr_y, line)| {
+            let mut curr_x = 0;
+            let mut line_sum = 0;
+            for num_str in line?.split(|c: char| !c.is_numeric()).collect::<Vec<_>>() {
+                if num_str.len() > 0 {
+                    // Search for symbol locations that match
+                    let min_val = match curr_x {
+                        0 => 0,
+                        _ => curr_x - 1
+                    };
+                    let max_val = curr_x + num_str.len();
+                    if exists(&symbol_locs[curr_y], min_val, max_val)
+                        || (curr_y > 0
+                            && exists(&symbol_locs[curr_y - 1], min_val, max_val))
+                        || (curr_y < symbol_locs.len() - 1
+                            && exists(&symbol_locs[curr_y + 1], min_val, max_val))
+                    {
+                        line_sum += num_str.parse::<i32>()?;
+                    }
+                }
+                curr_x += num_str.len() + 1;
+            }
+            Ok(line_sum)
+        })
+        .collect::<Result<Vec<i32>, Box<dyn Error>>>()?
+        .iter()
+        .sum();
+    // println!("{:?}", symbol_locs);
+    println!("{sum}");
+    Ok(())
+}
+
+fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
+}
---
+++ 23/src/03/b.rs
@@ -0,0 +1,99 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Lines};
+use std::path::Path;
+
+fn adjust_gear(sorted_list: &mut Vec<Gear>, adjustment: i32, min_val: usize, max_val: usize) {
+    for g in sorted_list.iter_mut() {
+        let x = g.x_index;
+        if x >= min_val {
+            if x <= max_val {
+                g.add_adjacent(adjustment);
+            }
+            if x > max_val {
+                break;
+            }
+        }
+    }
+}
+
+struct Gear {
+    // struct for all *potential* gears
+    pub x_index: usize,
+    ratio: i32,
+    adjacent: usize,
+}
+
+impl Gear {
+    pub fn new(x_index: usize) -> Self {
+        Self {
+            x_index,
+            ratio: 1,
+            adjacent: 0,
+        }
+    }
+
+    pub fn add_adjacent(&mut self, num: i32) {
+        self.ratio *= num;
+        self.adjacent += 1;
+    }
+
+    pub fn gear_ratio(&self) -> i32 {
+        if self.adjacent == 2 {
+            self.ratio
+        } else {
+            0
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = "src/03/input.txt";
+    let mut symbol_locs: Vec<_> = read_lines(filename)?
+        .into_iter()
+        .map(|line| {
+            Ok(line?
+                .char_indices()
+                .filter_map(|(i, c)| if c == '*' { Some(Gear::new(i)) } else { None })
+                .collect::<Vec<Gear>>())
+        })
+        .collect::<Result<Vec<_>, Box<dyn Error>>>()?;
+
+    for (curr_y, line) in read_lines(filename)?.into_iter().enumerate() {
+        let mut curr_x = 0;
+        for num_str in line?.split(|c: char| !c.is_numeric()).collect::<Vec<_>>() {
+            if num_str.len() > 0 {
+                // Search for symbol locations that match
+                let min_val = match curr_x {
+                    0 => 0,
+                    _ => curr_x - 1,
+                };
+                let max_val = curr_x + num_str.len();
+                let num = num_str.parse::<i32>()?;
+                adjust_gear(&mut symbol_locs[curr_y], num, min_val, max_val);
+                if curr_y > 0 {
+                    adjust_gear(&mut symbol_locs[curr_y - 1], num, min_val, max_val);
+                }
+                if curr_y < symbol_locs.len() - 1 {
+                    adjust_gear(&mut symbol_locs[curr_y + 1], num, min_val, max_val);
+                }
+            }
+            curr_x += num_str.len() + 1;
+        }
+    }
+    // println!("{:?}", symbol_locs);
+    let sum: i32 = symbol_locs
+        .iter()
+        .map(|gears| gears.iter().map(|gear| gear.gear_ratio()).sum::<i32>())
+        .sum();
+    println!("{sum}");
+    Ok(())
+}
+
+fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
+}
