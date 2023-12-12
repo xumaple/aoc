6f70b184705d511dbe00d00facaaf48df2459ee7
Maple Xu <mapxu@microsoft.com>
2023-12-12 07:55:50 +0000

    12a

--- 23/src/d12/a.rs
+++ 23/src/d12/a.rs
@@ -3,7 +3,7 @@ use super::*;
 #[aoc_proc::aoc_run(12a)]
 pub fn run(input: impl AsRef<str>) -> Result<IntType, BoxError> {
     let sum = input.as_ref().lines().map(|line| {
-        0
+        line.parse::<Record>().unwrap().num_combinations()
     }).sum();
     Ok(sum)
 }
--- 23/src/d12/mod.rs
+++ 23/src/d12/mod.rs
@@ -5,6 +5,89 @@ pub mod b;
 
 pub type IntType = u32;
 
+// #[derive(Debug)]
+// pub enum Spring {
+//     Operational,
+//     Damaged,
+//     Unknown,
+// }
+
+// impl UnsafeFrom<char> for Spring {
+//     fn ufrom(input: char) -> Self {
+//         match input {
+//             '.' => Self::Operational,
+//             '#' => Self::Damaged,
+//             '?' => Self::Unknown,
+//             _ => panic!(),
+//         }
+//     }
+// }
+
+#[derive(Debug)]
+pub struct Record(String, Vec<IntType>);
+
+impl FromStr for Record {
+    type Err = E;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (springs, counts) = s.ssplit_once(' ');
+        Ok(Self(
+            springs.to_string(),
+            counts.split(',').map(IntType::ufrom).collect_vec(),
+        ))
+    }
+}
+
+impl Record {
+    pub fn num_combinations(&self) -> IntType {
+        self.helper(self.0.clone())
+    }
+
+    fn helper(&self, springs: String) -> IntType {
+        let known_counts = Record::get_known_counts(springs.as_str());
+        if !self.counts_is_possible(&known_counts) {
+            return 0;
+        }
+        let pos = match springs.find('?') {
+            Some(pos) => pos,
+            None => { return if known_counts == self.1 { 1 } else { 0 }},
+        };
+        let mut s1 = springs.clone();
+        let mut s2 = springs.clone();
+        unsafe {
+            s1.as_mut_vec()[pos] = b'#';
+            s2.as_mut_vec()[pos] = b'.';
+        }
+        self.helper(s1) + self.helper(s2)
+    }
+
+    fn get_known_counts(s: &str) -> Vec<IntType> {
+        match s.split_once('?') {
+            Some((f, _)) => f,
+            None => s,
+        }.split('.').filter_map(|s| match s.len() {
+            0 => None,
+            i => Some(i as IntType)
+        }).collect_vec()
+    }
+
+    fn counts_is_possible(&self, counts: &Vec<IntType>) -> bool {
+        let len = counts.len();
+        if len == 0 {
+            return true;
+        }
+        if len > self.1.len() {
+            return false;
+        }
+        if counts[..len - 1] != self.1[..len - 1] {
+            return false;
+        }
+        if counts[len - 1] > self.1[len - 1] {
+            return false;
+        }
+        true
+    }
+}
+
 #[cfg(test)]
 mod test_a {
     use super::a::run;
@@ -12,7 +95,7 @@ mod test_a {
 
     #[test]
     fn sample() {
-        assert_eq!(run(read("src/d12/sample.txt").unwrap()).unwrap(), 0);
+        assert_eq!(run(read("src/d12/sample.txt").unwrap()).unwrap(), 21);
     }
 
     // #[test]
